5f2e10035ce4270bd465f29168c9ad404cc3a34a
GalactiCorpz <32594329+GalactiCorpz@users.noreply.github.com>
2018-10-13 14:22:18 +0200

    Finished 10 Minute Challenge
    Project Uploaded

---
+++ Assets/fruit.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class fruit : MonoBehaviour {
+
+    private void Update()
+    {
+        if(transform.position.y < -5)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+}
---
+++ Assets/player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+
+    public float SpawnTime;
+    public int Score;
+    public UnityEngine.UI.Text ScoreText;
+    public GameObject[] prefabs;
+
+    float spawnTime;
+    
+	void Update () {
+		if(spawnTime <= 0)
+        {
+            spawnTime = SpawnTime;
+            Instantiate(prefabs[Random.Range(0, prefabs.Length)], new Vector3(Random.Range(-5, 5), 5), Quaternion.identity, null);
+        }
+        spawnTime -= Time.deltaTime;
+
+        transform.position = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        ScoreText.text = Score.ToString();
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Fruit"))
+        {
+            Destroy(collision.gameObject);
+            Score++;
+        }
+    }
+}
